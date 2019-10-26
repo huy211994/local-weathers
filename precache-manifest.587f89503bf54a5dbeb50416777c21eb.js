857a77b03a5e1707512d59ddb5a6b8de52d6cfa5
huy211994 <huy211994@gmail.com>
2019-10-25 18:07:51 -0700

    Updates

---
+++ precache-manifest.587f89503bf54a5dbeb50416777c21eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6640e0ff9951438713ae8409fea96a22",
+    "url": "/local-weathers/index.html"
+  },
+  {
+    "revision": "8fed0be824c9b7b16b65",
+    "url": "/local-weathers/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "b1031e12563257077214",
+    "url": "/local-weathers/static/css/main.04d923c6.chunk.css"
+  },
+  {
+    "revision": "8fed0be824c9b7b16b65",
+    "url": "/local-weathers/static/js/2.0d237fc1.chunk.js"
+  },
+  {
+    "revision": "b1031e12563257077214",
+    "url": "/local-weathers/static/js/main.83d97ede.chunk.js"
+  },
+  {
+    "revision": "9659e767573733315e00",
+    "url": "/local-weathers/static/js/runtime-main.5e921134.js"
+  }
+]);
